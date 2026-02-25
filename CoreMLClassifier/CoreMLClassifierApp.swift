3bf01fbea82b8e884c0775dff68f8594423f6200
Lexter T <lexter.tapawan@gmail.com>
2026-02-25 12:24:48 -0800

    Initial Commit

---
+++ CoreMLClassifier/CoreMLClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLClassifierApp.swift
+//  CoreMLClassifier
+//
+//  Created by Lexter Tapawan on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
